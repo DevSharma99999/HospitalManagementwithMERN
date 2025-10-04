50c849983fde27e00abe74989488f6a816d2a1cd
DevSharma99999 <kaushikdev381@gmail.com>
2025-10-04 19:15:43 +0530

    Delete routes directory

--- routes/doctorLogin.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { doctorLoginAuth } from '../controller/doctorLogin.js';
-import { restrictTODoctor } from '../middleware/jwtAuthentication.js';
-
-export const doctorloginsuccessful = express.Router();
-
-doctorloginsuccessful.post("/doctor-login",doctorLoginAuth , restrictTODoctor);
